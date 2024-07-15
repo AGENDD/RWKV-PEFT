e95580b0943c5e3f70ee031872cae5b015ec1025
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-07-15 18:09:40 +0800

    7/15

--- train.py
+++ train.py
@@ -462,7 +462,7 @@ if __name__ == "__main__":
 
     from datasets import load_from_disk
     
-    dataset = load_from_disk("temp_datasests/en-final")
+    dataset = load_from_disk("temp_datasets/en-final")
     dataset = MyDataset(args, dataset)
     data_loader = DataLoader(dataset, shuffle=False, pin_memory=True, batch_size=args.micro_bsz, num_workers=1, persistent_workers=False, drop_last=True)
     
