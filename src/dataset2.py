5eb1e687ea804ac43e658f9bce75781786a42493
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-07-16 15:30:09 +0800

    7/16

--- src/dataset2.py
+++ src/dataset2.py
@@ -22,7 +22,7 @@ class MyDataset(Dataset):
 
     def __getitem__(self, idx):
         sample = self.hf_dataset[idx]
-        audio = torch.tensor(sample['speech']).half()
+        audio = sample['speech']
         answer = sample['text']
         
         # print(f"speech input{idx}:{len(audio)}")
