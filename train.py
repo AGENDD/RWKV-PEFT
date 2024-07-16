5ebb9767c853980fdd565981e8b190943ed95732
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-07-16 11:39:46 +0800

    7/16

--- train.py
+++ train.py
@@ -460,11 +460,6 @@ if __name__ == "__main__":
     # must set shuffle=False, persistent_workers=False (because worker is in another thread)
     # data_loader = DataLoader(train_data, shuffle=False, pin_memory=True, batch_size=args.micro_bsz, num_workers=1, persistent_workers=False, drop_last=True)
 
-    from datasets import load_from_disk
-    
-
-    
-    # trainer.fit(model, data_loader)
     
     from src.asr import SLAM_ASR
     Total_model = SLAM_ASR(
@@ -473,9 +468,12 @@ if __name__ == "__main__":
         model,
     )
     
+    from datasets import load_from_disk
     dataset = load_from_disk("temp_datasets/en-final")
-    dataset = MyDataset(args, dataset)
+    dataset = MyDataset(args, dataset[0])
     data_loader = DataLoader(dataset, shuffle=False, pin_memory=True, batch_size=args.micro_bsz, num_workers=1, persistent_workers=False, drop_last=True)
+
+    trainer.fit(model, data_loader)
 
     exit(0)
 
