9b7d9fa4a7f744aaa8d6d301ce5f2f988df56473
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-07-16 12:08:40 +0800

    7/16

--- train.py
+++ train.py
@@ -470,7 +470,7 @@ if __name__ == "__main__":
     
     from datasets import load_from_disk
     dataset = load_from_disk("temp_datasets/en-final")
-    dataset = MyDataset(args, dataset[0])
+    dataset = MyDataset(args, dataset[:10])
     data_loader = DataLoader(dataset, shuffle=False, pin_memory=True, batch_size=args.micro_bsz, num_workers=1, persistent_workers=False, drop_last=True)
 
     trainer.fit(Total_model, data_loader)
