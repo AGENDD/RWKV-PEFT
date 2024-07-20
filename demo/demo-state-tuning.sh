191bac52ea50f2d9faf2ed36cde1cd22694b6e8a
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-07-20 11:53:04 +0800

    7/20

--- demo/demo-state-tuning.sh
+++ demo/demo-state-tuning.sh
@@ -18,7 +18,7 @@ python train.py --load_model $load_model --devices 4 \
 --data_type binidx --vocab_size 65536 \
 --ctx_len $ctx_len --epoch_steps $epoch_steps --epoch_count 100 --epoch_begin 0 --epoch_save $epoch_save --micro_bsz $micro_bsz \
 --n_layer $n_layer --n_embd $n_embd \
---pre_ffn 0 --head_qk 0 --lr_init 1e-6 --lr_final 1e-5 --warmup_steps 5 --beta1 0.9 --beta2 0.99 --adam_eps 1e-8 \
+--pre_ffn 0 --head_qk 0 --lr_init 5e-6 --lr_final 1e-5 --warmup_steps 1 --beta1 0.9 --beta2 0.99 --adam_eps 1e-8 \
 --accelerator gpu --devices 1 --strategy deepspeed_stage_1 --grad_cp 1 \
 --precision bf16 \
 --my_testing "x060" \
