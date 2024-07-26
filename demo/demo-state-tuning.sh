170b0048ec3eb43b180340690598fffb99959042
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-07-26 17:51:14 +0800

    7/27

--- demo/demo-state-tuning.sh
+++ demo/demo-state-tuning.sh
@@ -16,7 +16,7 @@ export HF_ENDPOINT=https://hf-mirror.com
 python train.py --load_model $load_model --devices 4 \
 --proj_dir $proj_dir --data_file $data_file \
 --data_type binidx --vocab_size 65536 \
---ctx_len $ctx_len --epoch_steps $epoch_steps --epoch_count 1 --epoch_begin 0 --epoch_save $epoch_save --micro_bsz $micro_bsz \
+--ctx_len $ctx_len --epoch_steps $epoch_steps --epoch_count 3 --epoch_begin 0 --epoch_save $epoch_save --micro_bsz $micro_bsz \
 --n_layer $n_layer --n_embd $n_embd \
 --pre_ffn 0 --head_qk 0 --lr_init 1e-4 --lr_final 1e-4 --warmup_steps 100 --beta1 0.9 --beta2 0.99 --adam_eps 1e-8 \
 --accelerator gpu --strategy deepspeed_stage_1 --grad_cp 1 \
