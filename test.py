9fcff068f4733aa7e5084b14db1724ac71fa77b8
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-08-07 23:37:56 +0800

    st

--- test.py
+++ test.py
@@ -4,15 +4,15 @@ import soundfile as sf
 from tqdm import tqdm
 dataset = load_dataset('covost2','en_zh-CN',data_dir = 'temp_datasets/covost-en_zhCN')["train"].select(range(100))
 
-
+import resampy
 
 with open("temp_audios/text.txt",'w') as f:
     
     for i in tqdm(range(100)):    
         sample = dataset[i]
         audio = sample['audio']['array']
-        audio = librosa.resample(audio,orig_sr= 48000,target_sr= 16000)
-        
+        # audio = librosa.resample(audio,orig_sr= 48000,target_sr= 16000)
+        audio = resampy.resample(audio, 48000, 16000)
         transcription = sample['sentence']
         translation = sample["translation"]
         
