d250218762bd88ffb0dc9d572af3e94eb585cfaa
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-08-08 00:10:52 +0800

    st

--- test.py
+++ test.py
@@ -11,10 +11,10 @@ with open("temp_audios/text.txt",'w') as f:
     for i in tqdm(range(10)):    
         sample = dataset[i]
         audio = sample['audio']['array']
-        tqdm.write(f"before resample{len(audio)}")
+        tqdm.write(f"before resample:{len(audio)}")
         # audio = librosa.resample(audio,orig_sr= 48000,target_sr= 16000)
         audio = resampy.resample(audio, 48000, 16000)
-        tqdm.write(f"after resample{len(audio)}")
+        tqdm.write(f"after resample:{len(audio)}")
         transcription = sample['sentence']
         translation = sample["translation"]
         
