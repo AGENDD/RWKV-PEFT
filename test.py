6bf6e1c66e43d9eb62bab05b03acef3cae0dde46
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-08-08 00:42:56 +0800

    st

--- test.py
+++ test.py
@@ -13,15 +13,17 @@ with open("temp_audios/text.txt",'w') as f:
         sample = dataset[i]
         audio = sample['audio']['array']
         tqdm.write(f"before resample:{len(audio)}")
-        # audio = librosa.resample(audio,orig_sr= 48000,target_sr= 16000)
-        audio = resampy.resample(audio, 48000, 16000)
-        tqdm.write(f"after resample:{len(audio)}")
+        audio1 = librosa.resample(audio,orig_sr= 48000,target_sr= 16000)
+        audio2 = resampy.resample(audio, 48000, 16000)
+        tqdm.write(f"after resample1:{len(audio1)}")
+        tqdm.write(f"after resample2:{len(audio2)}")
         transcription = sample['sentence']
         translation = sample["translation"]
         
         f.write(f"{i}\n")
         f.write(f"{transcription}\n")
         f.write(f"{translation}\n")
-        write(f'temp_audios/{i}.wav', 16000, audio.astype('int16'))
+        sf.write(f'temp_audio/temp{i}_1.wav', audio1, 16000)
+        sf.write(f'temp_audio/temp{i}_2.wav', audio2, 16000)
         
         
