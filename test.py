17c6d74c997c98038c445e590bb0bed17c1e3a87
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-08-07 18:20:50 +0800

    st

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import librosa
 from datasets import load_from_disk,load_dataset, concatenate_datasets
 import soundfile as sf
+from tqdm import tqdm
 dataset = load_dataset('covost2','en_zh-CN',data_dir = 'temp_datasets/covost-en_zhCN')["train"].select(range(100))
 
 
 
 with open("temp_audios/text.txt",'w') as f:
     
-    for i in range(100):    
+    for i in tqdm(range(100)):    
         sample = dataset[i]
         audio = sample['audio']['array']
         audio = librosa.resample(audio,orig_sr= 48000,target_sr= 16000)
