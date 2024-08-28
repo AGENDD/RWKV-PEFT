d1709b7715f7f93c61e241c9125ed72ac14be511
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-08-28 21:11:17 +0800

    st

---
+++ dl_dataset.py
@@ -0,0 +1,14 @@
+from datasets import load_dataset
+
+arr = ['dutch','french','german','italian','polish','portuguese','spanish']
+
+for i in arr:
+    print(i)
+    while(True):
+        try:
+            mls = load_dataset("facebook/multilingual_librispeech", i, split="train",resume_download=True)
+            break
+        except:
+            continue
+
+print("finish")
