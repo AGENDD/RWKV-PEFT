756d4ff64810b345075bb155048b5b127fb3a0db
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-08-29 08:29:38 +0800

    st

--- dl_dataset.py
+++ dl_dataset.py
@@ -6,7 +6,7 @@ for i in arr:
     print(i)
     while(True):
         try:
-            mls = load_dataset("facebook/multilingual_librispeech", i, split="dev")
+            mls = load_dataset("facebook/multilingual_librispeech", i, split="9_hours")
             break
         except Exception as e:
             print(e)
