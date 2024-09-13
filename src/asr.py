ab08b5a0ec41816b05e541a89530773f8441a586
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-09-13 22:09:59 +0800

    qa

--- src/asr.py
+++ src/asr.py
@@ -110,9 +110,9 @@ class SLAM_ASR(pl.LightningModule):
         self.T_rwkv = 0
         
         self.TTS = TTS(language='EN', device=device).to(torch.float32)
-        wave = self.TTS.tts_to_file("This is a testing", self.TTS.hps.data.spk2id['EN-US'], None, speed=1.0)
-        print(wave)
-        exit(0)
+        # wave = self.TTS.tts_to_file("This is a testing", self.TTS.hps.data.spk2id['EN-US'], None, speed=1.0)
+        # print(wave)
+        # exit(0)
         # for name, param in self.TTS.named_parameters():
         #     print(f"Parameter name: {name}, Storage type: {param.dtype}")
         
@@ -393,9 +393,10 @@ class SLAM_ASR(pl.LightningModule):
     def forward(self, questions: List[str], transcriptions: List[str] = None):
         
         question_wave = []
-        
+        for name, param in self.TTS.named_parameters():
+            print(f"Parameter name: {name}, Storage type: {param.dtype}")
         for it in questions:
-            
+            self.TTS = self.TTS.to(torch.float32)
             wave = self.TTS.tts_to_file(it, self.TTS.hps.data.spk2id['EN-US'], None, speed=1.0)
             with io.BytesIO() as buffer:
                 sf.write(buffer, wave.astype(np.int16), 22050, format='WAV')
