5b65e48150635e1fe6e9a39dae219af9ae274f22
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-09-15 14:10:10 +0800

    qa

--- test.py
+++ test.py
@@ -1,16 +1,55 @@
-from datasets import load_dataset
+from datasets import load_dataset,load_from_disk
+from melo.api import TTS
+import io
+import soundfile as sf
+import resampy
+import numpy as np
+import sys
+from contextlib import contextmanager, redirect_stdout, redirect_stderr
+import os
+
+@contextmanager
+def suppress_stdout(*args, **kwargs):
+    with open(os.devnull, 'w') as devnull:
+        with redirect_stdout(devnull), redirect_stderr(devnull):
+            yield
 
 
-dataset = load_dataset("HuggingFaceH4/ultrachat_200k",split="train_sft")#207865
-        
-# 定义过滤函数
-def filter_long_prompts(example):
-    return len(example['prompt'].split()) <= 30
+dataset = load_from_disk("temp_datasets/ultrachat")
 
-# 过滤数据集，使用32个线程
-filtered_dataset = dataset.filter(filter_long_prompts, num_proc=32)
+TTS = TTS(language='EN', device='auto')
+speaker_ids = TTS.hps.data.spk2id
 
-print(len(filtered_dataset))
 
-filtered_dataset.save_to_disk("temp_datasets/ultrachat")
+def fun(example):
+    transcript = example['prompt']
+    
+    with suppress_stdout():
+        wave = TTS.tts_to_file(transcript, speaker_ids['EN-US'], None, speed=1.0)
+
+    with io.BytesIO() as buffer:
+        sf.write(buffer, wave.astype(np.int16), 44100, format='WAV')
+        buffer.seek(0)
+        wave, sr = sf.read(buffer, dtype='int16')
+        try:
+            example['speech'] = resampy.resample(wave, 44100, 16000)
+        except:
+            example['speech'] = None
+    return example
+
+dataset = dataset.map(fun,remove_columns=["prompt_id","messages"])
+
+def fil(example):
+    if(example['speech'] == None):
+        print(example['prompt'])
+        return False
+    else:
+        return True
+    
+dataset = dataset.filter(fil,num_proc=32)
+
+
+print(len(dataset))
+
+dataset.save_to_disk("temp_datasets/ultrachat_speech")
 
