d18865f9a8554e1a899ca48f746da35c0b0194a2
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-09-19 21:18:21 +0800

    qa

--- test.py
+++ test.py
@@ -13,76 +13,74 @@ import librosa
 from torchaudio.transforms import Resample
 from torchaudio import load, save
 
-# @contextmanager
-# def suppress_stdout(*args, **kwargs):
-#     with open(os.devnull, 'w') as devnull:
-#         with redirect_stdout(devnull), redirect_stderr(devnull):
-#             yield
+@contextmanager
+def suppress_stdout(*args, **kwargs):
+    with open(os.devnull, 'w') as devnull:
+        with redirect_stdout(devnull), redirect_stderr(devnull):
+            yield
 
 
-# dataset = load_from_disk("temp_datasets/ultrachat")
+dataset = load_from_disk("temp_datasets/ultrachat")
 
 TTS = TTS(language='EN', device='auto')
 speaker_ids = TTS.hps.data.spk2id
 
 
-# def fun(example):
-#     transcript = example['prompt']
+def fun(example):
+    try:
+        with suppress_stdout():
+            transcript = example['prompt']
+
+            wave = TTS.tts_to_file(transcript, speaker_ids['EN-US'], None, speed=1.0)
+            wave = torch.tensor(wave).unsqueeze(0)
+            resample = Resample(44100, 16000)
+            resampled_audio = resample(wave[0])
+            wave = resampled_audio.squeeze(0).numpy()
+        example['speech'] = wave
+    except:
+        example['speech'] = None
+    return example
+
+dataset = dataset.map(fun,remove_columns=["prompt_id","messages"])
+
+def fil(example):
+    if(example['speech'] == None):
+        print(example['prompt'])
+        return False
+    else:
+        return True
     
-#     try:
-#         with suppress_stdout():
-#             wave = TTS.tts_to_file(transcript, speaker_ids['EN-US'], None, speed=1.0)
-
-#         with io.BytesIO() as buffer:
-#             sf.write(buffer, wave.astype(np.int16), 44100, format='WAV')
-#             buffer.seek(0)
-#             wave, sr = sf.read(buffer, dtype='int16')
-
-#             example['speech'] = resampy.resample(wave, 44100, 16000)
-#     except:
-#         example['speech'] = None
-#     return example
-
-# dataset = dataset.map(fun,remove_columns=["prompt_id","messages"])
-
-# def fil(example):
-#     if(example['speech'] == None):
-#         print(example['prompt'])
-#         return False
-#     else:
-#         return True
-    
-# dataset = dataset.filter(fil,num_proc=32)
+dataset = dataset.filter(fil,num_proc=32)
 
 
-# print(len(dataset))
+print(len(dataset))
 
-# dataset.save_to_disk("temp_datasets/ultrachat_speech")
+dataset.save_to_disk("temp_datasets/ultrachat_speech")
 
 
-dataset = load_from_disk("temp_datasets/ultrachat").select(range(10))
+# dataset = load_from_disk("temp_datasets/ultrachat").select(range(10))
 
 
 
-for i,data in enumerate(dataset):
-    transcript = data['prompt']
+# for i,data in enumerate(dataset):
+#     transcript = data['prompt']
 
-    wave = TTS.tts_to_file(transcript, speaker_ids['EN-US'], None, speed=1.0)
-    sf.write("temp_audios/origin.wav",wave, 44100)
-    print(f"{len(wave)}:{wave}")
-    wave = torch.tensor(wave).unsqueeze(0)
-    resample = Resample(44100, 16000)
-    resampled_audio = resample(wave[0])
-    wave = resampled_audio.squeeze(0).numpy()
-    sf.write("temp_audios/resempled.wav",wave, 16000)
-    print(f"{len(wave)}:{wave}")
-    with io.BytesIO() as buffer:
-        sf.write(buffer, wave, 16000, format='WAV')
-        buffer.seek(0)
-        wave, sr = sf.read(buffer,dtype='float32')
-    print(f"{len(wave)}:{wave}")
-    sf.write("temp_audios/normal.wav",wave, sr)
+#     wave = TTS.tts_to_file(transcript, speaker_ids['EN-US'], None, speed=1.0)
+#     sf.write("temp_audios/origin.wav",wave, 44100)
+#     print(f"{len(wave)}:{wave}")
+#     wave = torch.tensor(wave).unsqueeze(0)
+#     resample = Resample(44100, 16000)
+#     resampled_audio = resample(wave[0])
+#     wave = resampled_audio.squeeze(0).numpy()
+#     sf.write("temp_audios/resempled.wav",wave, 16000)
+#     print(f"{len(wave)}:{wave}")
+#     with io.BytesIO() as buffer:
+#         sf.write(buffer, wave, 16000, format='WAV')
+#         buffer.seek(0)
+#         wave, sr = sf.read(buffer,dtype='float32')
+#     print(f"{len(wave)}:{wave}")
+#     sf.write("temp_audios/normal.wav",wave, sr)
 
     
-    break
+#     break
             
