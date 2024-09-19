58f00deadac2f13cd137cb4899d851b731cbe04c
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-09-19 18:57:03 +0800

    qa

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 from datasets import load_dataset,load_from_disk
 import torchaudio
 import torch
-# from melo.api import TTS
-# import io
+from melo.api import TTS
+import io
 import soundfile as sf
-# import resampy
-# import numpy as np
-# import sys
-# from contextlib import contextmanager, redirect_stdout, redirect_stderr
-# import os
+import resampy
+import numpy as np
+import sys
+from contextlib import contextmanager, redirect_stdout, redirect_stderr
+import os
 
 # @contextmanager
 # def suppress_stdout(*args, **kwargs):
@@ -57,17 +57,19 @@ import soundfile as sf
 # dataset.save_to_disk("temp_datasets/ultrachat_speech")
 
 
-dataset = load_from_disk("temp_datasets/ultrachat_speech").select(range(10))
+dataset = load_from_disk("temp_datasets/ultrachat").select(range(10))
 
 
 
 for i,data in enumerate(dataset):
-    
-    waveform = data['speech']
-    # waveform = torch.tensor(waveform)
-    sample_rate = 16000
-    sf.write(f"temp_audios/audio_{i}.wav", waveform, sample_rate)
+    transcript = data['prompt']
 
-
-
-
+    wave = TTS.tts_to_file(transcript, speaker_ids['EN-US'], None, speed=1.0)
+    sf.write("temp_audios/origin.wav",wave, 44100)
+    with io.BytesIO() as buffer:
+        sf.write(buffer, wave.astype(np.int16), 44100, format='WAV')
+        buffer.seek(0)
+        wave, sr = sf.read(buffer, dtype='int16')
+    sf.write("temp_audios/origin_16000.wav",wave, sr)
+    break
+            
