e2a9c7bca3caefee0de62cd4cd8c458411a0f81e
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-10-10 11:37:18 +0800

    qa

--- dl_dataset.py
+++ dl_dataset.py
@@ -1,3 +1,8 @@
 from datasets import load_dataset
 token = "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"
-ds = load_dataset("gpt-omni/VoiceAssistant-400K", token = token)
+
+
+while(True):
+    ds = load_dataset("gpt-omni/VoiceAssistant-400K", token = token, num_proc=32)
+    break
+print("finish")
