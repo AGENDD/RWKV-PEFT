f97572958e18aa8562cb5df16efeca14ef08493f
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-10-10 13:58:43 +0800

    qa

--- dl_dataset.py
+++ dl_dataset.py
@@ -4,7 +4,7 @@ token = "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"
 
 while(True):
     try:
-        ds = load_dataset("gpt-omni/VoiceAssistant-400K", token = token, num_proc=32)
+        ds = load_dataset("gpt-omni/VoiceAssistant-400K", token = token, num_proc=8)
         break
     except:
         continue
