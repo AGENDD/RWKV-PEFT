2603b7e76802cd9589d37c5cfbe6826ce6a274e4
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-10-19 13:37:08 +0800

    qa

--- dl_dataset.py
+++ dl_dataset.py
@@ -1,10 +1,63 @@
 from datasets import load_dataset, load_from_disk
+from melo.api import TTS
+from torchaudio.transforms import Resample
+from torchaudio import load, save
+import torch
+from contextlib import contextmanager, redirect_stdout, redirect_stderr
+import os
+
+
 token = "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"
 
 ds = load_from_disk("temp_datasets/ultrachat")
 
-print(ds)
-print(ds[0])
+TTS = TTS(language='EN', device='auto')
+speaker_ids = TTS.hps.data.spk2id
+
+
+@contextmanager
+def suppress_stdout(*args, **kwargs):
+    with open(os.devnull, 'w') as devnull:
+        with redirect_stdout(devnull), redirect_stderr(devnull):
+            yield
+
+def mapp(data):
+    messages = data['messages']
+    count = 0
+    
+    speech_messages = []
+    for message in messages:
+        if(message["role"] == "assistant"):
+            assistantdic = {}
+            assistantdic['role'] = 'assistent'
+            assistantdic['content'] = message['content']
+            speech_messages.append(assistantdic)
+        else:
+            leng = len(message["content"].split())
+            if(leng > 30):
+                break
+            else:
+                with suppress_stdout():
+                    wave = TTS.tts_to_file(message["content"], speaker_ids['EN-US'], None, speed=1.0)
+                    wave = torch.tensor(wave).unsqueeze(0)
+                    resample = Resample(44100, 16000)
+                    resampled_audio = resample(wave[0])
+                    wave = resampled_audio.squeeze(0).numpy()
+                userdic = {}
+                userdic['content'] = wave
+                userdic['role'] = "user"
+                userdic['transcript'] = message['content']
+                speech_messages.append(userdic)
+                count += 1
+    
+    data["turns"] = count
+    data['speech_messages'] = speech_messages
+    
+    return data
+
+ds = ds.map(mapp, remove_columns=['messages', 'prompt', 'promt_id'])
+
+ds.save_to_disk("temp_datasets/ultrachat_speech_multiTurns")
 
 
 print("finish")
