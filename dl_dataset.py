669ff9c3371606e65bc32b33e2ae266bc1bc0ed1
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-10-21 12:05:09 +0800

    qa

--- dl_dataset.py
+++ dl_dataset.py
@@ -7,65 +7,69 @@ from contextlib import contextmanager, redirect_stdout, redirect_stderr
 import os
 
 
-token = "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"
+# token = "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"
 
-ds = load_from_disk("temp_datasets/ultrachat")
+# ds = load_from_disk("temp_datasets/ultrachat")
 
-TTS = TTS(language='EN', device='auto')
-speaker_ids = TTS.hps.data.spk2id
+# TTS = TTS(language='EN', device='auto')
+# speaker_ids = TTS.hps.data.spk2id
 
 
-@contextmanager
-def suppress_stdout(*args, **kwargs):
-    with open(os.devnull, 'w') as devnull:
-        with redirect_stdout(devnull), redirect_stderr(devnull):
-            yield
+# @contextmanager
+# def suppress_stdout(*args, **kwargs):
+#     with open(os.devnull, 'w') as devnull:
+#         with redirect_stdout(devnull), redirect_stderr(devnull):
+#             yield
 
-def mapp(data):
-    messages = data['messages']
-    count = 0
+# def mapp(data):
+#     messages = data['messages']
+#     count = 0
     
-    speech_messages = []
-    respond_messages = []
-    for message in messages:
-        if(message["role"] == "assistant"):
-            assistantdic = {}
-            assistantdic['role'] = 'assistent'
-            assistantdic['content'] = message['content']
-            respond_messages.append(assistantdic)
-        else:
-            leng = len(message["content"].split())
-            if(leng > 30):
-                break
-            else:
-                try:
-                    with suppress_stdout():
-                        wave = TTS.tts_to_file(message["content"], speaker_ids['EN-US'], None, speed=1.0)
-                        wave = torch.tensor(wave).unsqueeze(0)
-                        resample = Resample(44100, 16000)
-                        resampled_audio = resample(wave[0])
-                        wave = resampled_audio.squeeze(0).numpy()
+#     speech_messages = []
+#     respond_messages = []
+#     for message in messages:
+#         if(message["role"] == "assistant"):
+#             assistantdic = {}
+#             assistantdic['role'] = 'assistent'
+#             assistantdic['content'] = message['content']
+#             respond_messages.append(assistantdic)
+#         else:
+#             leng = len(message["content"].split())
+#             if(leng > 30):
+#                 break
+#             else:
+#                 try:
+#                     with suppress_stdout():
+#                         wave = TTS.tts_to_file(message["content"], speaker_ids['EN-US'], None, speed=1.0)
+#                         wave = torch.tensor(wave).unsqueeze(0)
+#                         resample = Resample(44100, 16000)
+#                         resampled_audio = resample(wave[0])
+#                         wave = resampled_audio.squeeze(0).numpy()
                         
-                    if(len(wave) / 16000 > 15.0):
-                        break
+#                     if(len(wave) / 16000 > 15.0):
+#                         break
                     
-                    userdic = {'content': wave, 'role': "user", 'transcript': message['content']}
-                    speech_messages.append(userdic)
-                    count += 1
-                except Exception as e:
-                    print(f"Error processing message: {e}")
-                    print(f"data: {message}")
-                    count = -1
-                    break
+#                     userdic = {'content': wave, 'role': "user", 'transcript': message['content']}
+#                     speech_messages.append(userdic)
+#                     count += 1
+#                 except Exception as e:
+#                     print(f"Error processing message: {e}")
+#                     print(f"data: {message}")
+#                     count = -1
+#                     break
     
-    data["turns"] = count
-    data['speech_messages'] = speech_messages
-    data['respond_messages'] = respond_messages
-    return data
+#     data["turns"] = count
+#     data['speech_messages'] = speech_messages
+#     data['respond_messages'] = respond_messages
+#     return data
 
-ds = ds.map(mapp, remove_columns=['messages', 'prompt', 'prompt_id'])
+# ds = ds.map(mapp, remove_columns=['messages', 'prompt', 'prompt_id'])
 
-ds.save_to_disk("temp_datasets/ultrachat_speech_multiTurns_unfiltered")
+# ds.save_to_disk("temp_datasets/ultrachat_speech_multiTurns_unfiltered")
+
+
+ds = load_from_disk("temp_datasets/ultrachat_speech_multiTurns_unfiltered")
+
 
 def fil(data):
     if(data["turns"] == -1):
@@ -73,7 +77,7 @@ def fil(data):
     
     return True
 
-ds = ds.filter(fil, num_proc=32)
+ds = ds.filter(fil)
 ds.save_to_disk("temp_datasets/ultrachat_speech_multiTurns")
 
 
