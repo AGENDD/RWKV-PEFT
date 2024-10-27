74fbbfa361b8ef1a65c20f230439034e52f1c49e
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-10-27 16:05:57 +0800

    qa

--- ul_dataset.py
+++ ul_dataset.py
@@ -2,7 +2,7 @@ from datasets import load_from_disk,DatasetDict
 
 dataset = load_from_disk("temp_datasets/ultrachat_tensor_10000")
 
-token = "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"
+token = "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" #write
 
 
 from huggingface_hub import login
