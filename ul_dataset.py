79acc5b2cc8ed4bada9b9de4efa263c354e90b3f
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-10-27 16:02:34 +0800

    qa

---
+++ ul_dataset.py
@@ -0,0 +1,17 @@
+from datasets import load_from_disk,DatasetDict
+
+dataset = load_from_disk("temp_datasets/ultrachat_tensor_10000")
+
+token = "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"
+
+
+from huggingface_hub import login
+
+# 登录 Hugging Face
+login('your_huggingface_api_token')
+
+# 创建 DatasetDict 对象
+dataset_dict = DatasetDict({'train': dataset})
+
+# 上传数据集
+dataset_dict.push_to_hub('ultrachat_tensor_10k')
