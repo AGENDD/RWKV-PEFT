482734ed3581f0a6b7b07dd03a754640502d7b76
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-10-28 19:44:42 +0800

    qa

--- ul_dataset.py
+++ ul_dataset.py
@@ -1,6 +1,6 @@
 from datasets import load_from_disk,DatasetDict
 
-dataset = load_from_disk("temp_datasets/ultrachat_tensor_10000")
+dataset = load_from_disk("temp_datasets/ultrachat_speech_multiTurns")
 
 token = "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" #write
 
@@ -16,7 +16,7 @@ dataset_dict = DatasetDict({'train': dataset})
 # 上传数据集
 while(True):
     try:
-        dataset_dict.push_to_hub('ultrachat_tensor_10k')
+        dataset_dict.push_to_hub('ultrachat_speech_multiTurns')
         break
     except Exception as e:
         print(e)
