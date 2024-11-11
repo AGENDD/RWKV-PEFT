9458c10ca30f95c05057082f97df0dfed5571c85
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-11-11 16:47:32 +0800

    qa

--- src/dataset2.py
+++ src/dataset2.py
@@ -30,10 +30,10 @@ class MyDataset(Dataset):
         while(True):
             try:
                 sample = self.hf_dataset[idx]
-                # if(len(sample['speech'])/16000 > 15.0 or len(sample['answer']) > 1500):
-                #     print("skip data")
-                #     idx = idx+1
-                #     continue    
+                if(len(sample['speech'])/16000 > 15.0 or len(sample['answer']) > 1500):
+                    print("skip data")
+                    idx = idx+1
+                    continue    
                 break
             except:
                 idx = idx+1
