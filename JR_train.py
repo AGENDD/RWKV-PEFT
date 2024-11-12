c06a91bcc4c4300299860da63002b7572415ff80
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-11-12 15:46:19 +0800

    qa

--- JR_train.py
+++ JR_train.py
@@ -585,8 +585,8 @@ if __name__ == "__main__":
         import soundfile as sf
         # con_dataset = load_from_disk("temp_datasets/VoiceAssistant")
         
-        con_dataset = load_from_disk("temp_datasets/ZHEN_mixed_filtered").shuffle()
-        
+        # con_dataset = load_from_disk("temp_datasets/ZHEN_mixed_filtered").shuffle()
+        con_dataset = load_from_disk("temp_datasets/chinese_speech").shuffle()
         tokenizer = Total_model.return_tokenizer()
         # Total_model = Total_model.to("cuda", dtype=torch.bfloat16)
         Total_model = Total_model.to("cuda", dtype=torch.bfloat16)
