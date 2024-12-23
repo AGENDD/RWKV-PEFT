0aac2d777472949dc7c76fbe6f193703a34e2ea6
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-12-23 22:41:46 +0800

    qa

--- JR_train.py
+++ JR_train.py
@@ -590,7 +590,8 @@ if __name__ == "__main__":
         # con_dataset = load_from_disk("temp_datasets/VoiceAssistant")
         
         # con_dataset = load_from_disk("temp_datasets/ZHEN_mixed_filtered").shuffle()
-        con_dataset = load_from_disk("temp_datasets/chinese_speech").shuffle()
+        # con_dataset = load_from_disk("temp_datasets/chinese_speech").shuffle()
+        con_dataset = load_dataset("carlot/AIShell",split="test").shuffle()
         # con_dataset, transcipt = aishell('test')
         
         
@@ -603,9 +604,9 @@ if __name__ == "__main__":
         
         # with open("temp_audios/text.txt",'w') as f:
         for data in con_dataset:
-            pattern = re.compile(r'[a-zA-Z+=-]')
-            if(pattern.search(data['trascript'])):
-                continue
+            # pattern = re.compile(r'[a-zA-Z+=-]')
+            # if(pattern.search(data['trascript'])):
+            #     continue
             # inputs = torch.tensor(data['speech']).to("cuda", torch.bfloat16)
             # path = 'temp_datasets/aishell/data_aishell/wav/train/'
             # path = 'temp_datasets/aishell/data_aishell/wav/test/'
@@ -613,11 +614,11 @@ if __name__ == "__main__":
             # inputs = librosa.resample(audio.astype(float), orig_sr=sr, target_sr=16000)
             # answer = transcipt[data]
             # answer = answer.replace(" ","")
-            inputs = data['speech']
-            answer = data['answer']
+            inputs = data['audio']['array']
+            answer = data['transcription']
+            answer = answer.replace(" ", "")
             
-            
-            print(f"questions:\n{data['trascript']}")
+            # print(f"questions:\n{data['trascript']}")
             print(f"true answer:\n{answer[:100]}...")
             print()
             print("predict:")
