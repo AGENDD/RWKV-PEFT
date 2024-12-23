1f88bc91859c780e737f7491831b346b65a11929
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-12-23 20:17:01 +0800

    qa

--- src/speech_encoder2.py
+++ src/speech_encoder2.py
@@ -64,7 +64,7 @@ class SpeechEncoder(nn.Module):
         """
         if train_mode == "adapter":
             for param in self.model.parameters():
-                param.requires_grad = False
+                param.requires_grad = True
             for param in self.adapter.parameters():
                 param.requires_grad = True
             
