c0e78c34d8f9cc471380ef312222ecf1a454db5c
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-12-26 16:55:18 +0800

    qa

--- dl_dataset.py
+++ dl_dataset.py
@@ -5,13 +5,15 @@ import torch
 from contextlib import contextmanager, redirect_stdout, redirect_stderr
 import os
 
+from tqdm import tqdm
+import numpy as np
+import soundfile as sf
 
 # token = "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"
-while(True):
-    try:
-        ds = load_dataset("carlot/AIShell", cache_dir='temp_datasets')
-        break
-    except Exception as e:
-        print(e)
 
-print(ds)
+ds = load_from_disk("temp_datasets/VoiceAssistant")
+
+for i in tqdm(range(100)):
+    array = ds[i]["question_audio"]['array']
+    sf.write('temp_audios/audio{i}.wav', array, 22050)
+
