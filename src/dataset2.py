e86c0ca0ba02714caacbe4cc7dfd8e48e0bbee73
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-12-27 13:27:36 +0800

    qa

--- src/dataset2.py
+++ src/dataset2.py
@@ -33,8 +33,12 @@ class MyDataset(Dataset):
                 sample = self.hf_dataset[idx]
                 # pattern = re.compile(r'[a-zA-Z+=-]')
                 assert type(sample["speech"]) == type(sample["speech_cosy"])
-                if(len(sample['speech_cosy'][0])/16000 > 13.0 or len(sample['answer']) > 1200):
-                    print("skip data")
+                if(len(sample['speech_cosy'][0])/16000 > 13.0):
+                    print("skip data audio too long")
+                    idx = idx+1
+                    continue
+                elif(len(sample['answer']) > 1200):
+                    print("skip data answer too long")
                     idx = idx+1
                     continue
                 # elif(pattern.search(sample['trascript'])):
