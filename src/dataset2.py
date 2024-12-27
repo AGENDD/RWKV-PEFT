42810483a3c2c55040c8e3428428ecbee169cbf1
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-12-27 15:37:34 +0800

    qa

--- src/dataset2.py
+++ src/dataset2.py
@@ -37,7 +37,7 @@ class MyDataset(Dataset):
                     print("skip data audio too long")
                     idx = idx+1
                     continue
-                elif(len(sample['answer']) > 1500):
+                elif(len(sample['answer']) > 1700):
                     print("skip data answer too long")
                     idx = idx+1
                     continue
