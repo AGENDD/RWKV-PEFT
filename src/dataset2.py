d48e14524f2bb16ed6f4fdb71a975575b439be2c
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2025-01-01 15:46:12 +0800

    qa

--- src/dataset2.py
+++ src/dataset2.py
@@ -32,11 +32,11 @@ class MyDataset(Dataset):
             try:
                 sample = self.hf_dataset[idx]
                 
-                # assert type(sample["speech"]) == type(sample["speech_cosy"])
-                # if(len(sample['speech_cosy'][0])/16000 > 15.0):
-                #     print("skip data audio too long")
-                #     idx = idx+1
-                #     continue
+                assert type(sample["speech"]) == type(sample["speech_cosy"])
+                if(len(sample['speech_cosy'][0])/16000 > 15.0):
+                    print("skip data audio too long")
+                    idx = idx+1
+                    continue
                 # elif(len(sample['answer']) > 1700):
                 #     print("skip data answer too long")
                 #     idx = idx+1
